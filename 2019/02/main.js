a7b76a8d72cca198adc0effbfdbae672c36d8126
Margaret Wang <mmwang@etsy.com>
2019-12-24 11:54:30 -0500

    Day 2 part 1

--- 2019/02/main.js
+++ 2019/02/main.js
@@ -9,6 +9,41 @@ const readInterface = readline.createInterface({
     console: false
 });
 
-let readInput = function() {
-    
+// Reads input as a comma separated string
+let readInput = () => {
+    return new Promise((resolve) => {
+        let contents;
+        readInterface
+        .on('line', (line) => {
+            // Convert current values to an array
+            contents = line.split(",").map((val) => { return +val; });
+        })
+        .on('close', () => {
+            resolve(contents);
+        })
+    });
 }
+
+let runIntcode = (array) => {
+    return array.map((cur, i) => {
+        if (i % 4 === 0) {
+            if (cur === 99) return;
+
+            let second = array[i+1];
+            let third = array[i+2];
+            let position = array[i+3];
+            console.log(cur, i, second, third, position);
+            if (cur === 1) {
+                array[position] = array[second] + array[third];
+            } else if (cur === 2) {
+                array[position] = array[second] * array[third];
+            }
+            return array[position]
+        }
+    });
+}
+
+readInput().then((contents) => {
+    runIntcode(contents);
+    console.log("Contents: ", contents);
+})
