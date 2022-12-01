120f64659dfb9c5ba0982355135147865eb4c3d4
margaret <mwang27@gmail.com>
2022-12-01 12:11:00 +0000

    Second part of day 1, including max heap

--- 2022/01/main.js
+++ 2022/01/main.js
@@ -31,9 +31,67 @@ const findTopSum = () => {
   return maxSum;
 }
 
-const findTopKSum = () => {
+const findTopKSum = (k = 3) => {
   let contents = readFile(inputLoc);
+  let maxThreeHeap = new MaxHeap(k);
 
+  let maxSum = 0;
+  let curSum = 0;
+
+  for (let i = 0; i < contents.length; i++) {
+    if (contents[i] === 0) {
+      curSum = 0;
+    }
+    if (curSum + contents[i] > maxSum) {
+      maxSum = curSum + contents[i];
+      maxThreeHeap.addToHeap(maxSum, maxThreeHeap);
+    }
+    curSum = curSum + contents[i];
+  }
+
+  let total = maxThreeHeap.getHeap().reduce((prev, cur) => { return prev = prev + cur }, 0);
+  console.log("Final max: ", maxThreeHeap.getHeap(), total);
 }
 
-console.log(findTopSum());
+/**
+ * MaxHeap
+ * Lessons:
+ * 1. Must always keep track of size and capacity, and add until you fill capacity
+ * 2. Must always update from the bottom, replacing with parent
+ */
+class MaxHeap {
+  constructor(k = 3) {
+    this.size = 0;
+    this.capacity = k;
+    this.heap = new Array(k).fill(0);  
+  }
+  getSize () {
+    return this.size;
+  }
+  getHeap() {
+    return this.heap;
+  }
+  addToHeap(num) {
+    if (this.size < this.capacity) {
+      this.heap[this.size] = num;
+      this.size++;
+    } else {
+      if (this.heap[this.size-1] < num) {
+        this.heap[this.size-1] = num;
+        this.updateHeap()
+      }
+    }
+  }
+  updateHeap() {
+    for (let i = this.heap.length - 1; i >= 0; i--) {
+      let parent = Math.floor((i)/2);
+      if (this.heap[parent] < this.heap[i]) {
+        [this.heap[parent], this.heap[i]] = [this.heap[i], this.heap[parent]];
+      }
+    }
+  }
+}
+
+
+// console.log(findTopSum());
+console.log(findTopKSum(3));
