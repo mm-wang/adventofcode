0266bc89cb3fea18a383504bb9b12da01f71591b
margaret <mwang27@gmail.com>
2022-12-02 17:05:57 +0000

    Day 2 first part

---
+++ 2022/02/main.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+
+const inputLoc = __dirname + "/input";
+
+const readFile = (filepath) => {
+  let buffer = fs.readFileSync(filepath);
+  const input = buffer.toString().split("\n")
+    .map((val) => {if (val.length) return val.split(' ');})
+    .filter(Boolean);
+  return input;
+}
+
+// Paper beats rock (1-0), Scissors beats paper (2-1), Rock beats scissors (0-2)
+const Play = {
+  Rock: 0,
+  Paper: 1,
+  Scissors: 2,
+}
+
+const first = {
+  A: Play.Rock,
+  B: Play.Paper,
+  C: Play.Scissors,
+}
+
+const second = {
+  X: Play.Rock,
+  Y: Play.Paper,
+  Z: Play.Scissors,
+}
+
+const shape = {
+  X: 1,
+  Y: 2,
+  Z: 3
+}
+
+const calculatePlayStrategy = () => {
+  const plays = readFile(inputLoc);
+  let outcome = -Number.MAX_SAFE_INTEGER;
+  let outcomeScore = 0;
+  let total = 0;
+  for (let i = 0; i < plays.length; i++) {
+    let opponent = plays[i][0];
+    let you = plays[i][1];
+    outcome = second[you] - first[opponent];
+    outcomeScore = calculateOutcomeScore(outcome);
+    total += shape[you] + outcomeScore;
+  }
+
+  return total;
+}
+
+const calculateOutcomeScore = (outcome) => {
+  switch (outcome)  {
+    case 1:
+      return 6;
+    case -2:
+      return 6;
+    case 0:
+      return 3;
+    default:
+      return 0;
+  }
+}
+
+console.log(calculatePlayStrategy());
